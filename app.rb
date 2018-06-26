a1c777a9e2dacfcb4888bf5182e4b0d903dc9684
btokraks22 <bree.v.v.13@gmail.com>
2018-06-26 16:18:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  
+  get '/' do
+   "I love Bok Choy!!"
+  
+  
+  
 end
+
+get '/nutritious'do
+  "Has lots of fiber"
+end
+
+
+end 
